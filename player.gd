06d3ae04deb1531c878c579b5dc8bdc12e863159
iammister1 <jackmalczewski927@gmail.com>
2026-04-24 17:40:41 +1000

    added walls and up and down

--- player.gd
+++ player.gd
@@ -2,21 +2,19 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300
 
 
-func _physics_process(delta: float) -> void:
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(_delta: float) -> void:
+	var input_vector := Vector2.ZERO
+	
+	input_vector.x = Input.get_axis("Left", "Right")
+	input_vector.y = Input.get_axis("up", "Down")
+	
+	if input_vector.length() > 0:
+		input_vector = input_vector.normalized()
+		velocity = input_vector * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2.ZERO
 
 	move_and_slide()
